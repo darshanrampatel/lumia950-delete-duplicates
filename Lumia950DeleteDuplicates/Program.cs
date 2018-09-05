02e243fc9a336ea968ffc96755459ae5da8f93ac
Darshan <darshanp@outlook.com>
2018-09-05 20:43:29 +0100

    Initial add

---
+++ Lumia950DeleteDuplicates/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Lumia950DeleteDuplicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Running...");
+            Console.WriteLine();
+            var folderPath = Path.Combine(Environment.GetEnvironmentVariable("USERPROFILE"), @"OneDrive\Pictures\Camera Roll");
+            var files = Directory.EnumerateFiles(folderPath, "*");
+            var filesHash = new HashSet<string>();
+            foreach (var file in files)
+            {
+                var fileName = Path.GetFileName(file);
+                if (fileName.StartsWith("WP"))
+                {
+                    filesHash.Add(fileName);
+                }
+            }
+            Console.WriteLine($"Found {filesHash.Count} WP files in {folderPath}");
+
+            Console.WriteLine("Press [Enter] to exit...");
+            Console.ReadLine();
+        }
+    }
+}
+
